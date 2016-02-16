48bbe8077091b74058b2ae5054588ed16203b126
sylar <sylar@Sylar-Mac-Mini-2.local>
2016-02-16 09:53:43 +0800

    手势

--- testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/fingerTap/FingerTapVCTL.m
+++ testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/fingerTap/FingerTapVCTL.m
@@ -22,7 +22,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    _viewFinger.multipleTouchEnabled = YES;
+//    _viewFinger.multipleTouchEnabled = YES;
     
     [self addGesture];
 }
@@ -30,51 +30,79 @@
 - (void)addGesture
 {
 //    UITapGestureRecognizer *tap1 = [[UITapGestureRecognizer alloc] init];
-}
-
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    NSLog(@"---------------------------");
-    for (UITouch *each_touch in touches)
-    {
-        CGPoint location = [each_touch locationInView:_viewFinger];
-        NSLog(@"location = %.2f, %.2f", location.x, location.y);
-    }
-}
-
-- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    NSLog(@"-------------%ld--------------", event.allTouches.count);
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture)];
+    [_viewFinger addGestureRecognizer:pan];
     
-    for (UITouch *each_touch in touches)
-    {
-        CGPoint location = [each_touch locationInView:_viewFinger];
-        NSLog(@"move = %.2f, %.2f", location.x, location.y);
-    }
-    if ( event.allTouches.count == 2)
-    {
-        CGPoint l1 = CGPointZero;
-        CGPoint l2 = CGPointZero;
-        BOOL first_object = YES;
-        for (UITouch *each_touch in event.allTouches)
-        {
-            if (first_object)
-            {
-                l1 = [each_touch locationInView:_viewFinger];
-                first_object = NO;
-            }
-            else
-            {
-                l2 = [each_touch locationInView:_viewFinger];
-            }
-        }
-        
-        CGPoint new_point = CGPointMake(l1.x/2+l2.x/2, l1.y/2+l2.y/2);
-        _viewPoint.center = new_point;
-        
-    }
+    UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeGesture)];
+    swipe.direction = UISwipeGestureRecognizerDirectionUp|UISwipeGestureRecognizerDirectionDown|UISwipeGestureRecognizerDirectionLeft;
+    [_viewFinger addGestureRecognizer:swipe];
+    
+    UIPinchGestureRecognizer *pinch = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinchGesture:)];
+    [_viewFinger addGestureRecognizer:pinch];
 }
+
+
+
+#pragma mark - gesture
+- (void)panGesture
+{
+    NSLog(@"pan");
+}
+
+- (void)pinchGesture:(UIPinchGestureRecognizer *)gesture
+{
+    NSLog(@"pinch = %f", gesture.scale);
+    _viewPoint.center = [gesture locationInView:_viewFinger];
+}
+
+- (void)swipeGesture
+{
+    NSLog(@"swipe");
+}
+
+//#pragma mark - touch event
+//-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+//{
+//    NSLog(@"---------------------------");
+//    for (UITouch *each_touch in touches)
+//    {
+//        CGPoint location = [each_touch locationInView:_viewFinger];
+//        NSLog(@"location = %.2f, %.2f", location.x, location.y);
+//    }
+//}
+//
+//- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+//{
+//    NSLog(@"-------------%ld--------------", event.allTouches.count);
+//    
+//    for (UITouch *each_touch in touches)
+//    {
+//        CGPoint location = [each_touch locationInView:_viewFinger];
+//        NSLog(@"move = %.2f, %.2f", location.x, location.y);
+//    }
+//    if ( event.allTouches.count == 2)
+//    {
+//        CGPoint l1 = CGPointZero;
+//        CGPoint l2 = CGPointZero;
+//        BOOL first_object = YES;
+//        for (UITouch *each_touch in event.allTouches)
+//        {
+//            if (first_object)
+//            {
+//                l1 = [each_touch locationInView:_viewFinger];
+//                first_object = NO;
+//            }
+//            else
+//            {
+//                l2 = [each_touch locationInView:_viewFinger];
+//            }
+//        }
+//        
+//        CGPoint new_point = CGPointMake(l1.x/2+l2.x/2, l1.y/2+l2.y/2);
+//        _viewPoint.center = new_point;
+//        
+//    }
+//}
 
 
 

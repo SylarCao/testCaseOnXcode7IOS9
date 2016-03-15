ab5612ee9a0a0842a2884667773e223c7ca45e75
sylar <sylar@Sylar-Mac-Mini-2.local>
2016-03-15 10:32:19 +0800

    自带的 UIActivityIndicatorView 相当于 progressed

--- testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/MBProgressHud/MBProgressHudVCTL.m
+++ testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/MBProgressHud/MBProgressHudVCTL.m
@@ -13,6 +13,10 @@
 
 @property (nonatomic, strong) MBProgressHUD *hud;
 
+@property (nonatomic, weak) IBOutlet UIView *v1;
+
+@property (nonatomic, strong) UIActivityIndicatorView *v2;
+
 @end
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 @implementation MBProgressHudVCTL
@@ -20,16 +24,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    [self fun1];
 }
 
 
 - (IBAction)btn1:(id)sender
 {
-    [self showHudAnimation];
-    [self hideHudAnimation];
-    [self showHudWithTitle];
+//    [self showHudAnimation];
+//    [self hideHudAnimation];
+//    [self showHudWithTitle];
+//    [_v2 stopAnimating];
+    
+//    [_v1 bringSubviewToFront:_v2];
+    [_v2 stopAnimating];
+    
+    
 }
 
+- (void)fun1
+{
+    UIActivityIndicatorView *in_view = [[UIActivityIndicatorView alloc] initWithFrame:_v1.bounds];
+    [_v1 addSubview:in_view];
+//    [in_view startAnimating];
+//    _v2 = in_view;
+    
+    // add button
+    UIButton *b1 = [UIButton buttonWithType:UIButtonTypeSystem];
+    b1.frame = CGRectMake(200, 100, 50, 50);
+    [b1 setTitle:@"bv1" forState:UIControlStateNormal];
+    [_v1 addSubview:b1];
+}
+
+- (IBAction)btn2:(id)sender
+{
+    NSLog(@"tap");
+}
 
 - (void) showHudWithTitle
 {

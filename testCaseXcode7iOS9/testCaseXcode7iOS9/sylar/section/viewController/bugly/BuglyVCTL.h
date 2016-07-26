7e6c5fce14ac52d85fa93499c96e2e1f6384db9c
sylar <sylar@Sylar-Mac-Mini-2.local>
2016-07-26 17:11:20 +0800

    bugly  注释

--- testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/bugly/BuglyVCTL.h
+++ testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/bugly/BuglyVCTL.h
@@ -10,6 +10,10 @@
 #import "BasicVCTL.h"
 
 // reference link: https://bugly.qq.com/v2/
+//  添加依赖库
+//  SystemConfiguration.framework
+//  Security.framework
+//  libz.dylib
 
 @interface BuglyVCTL : BasicVCTL
 

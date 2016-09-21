f59868a5924eb7636389886265edee93616a9532
sylar <sylar@Sylar-Mac-Mini-2.local>
2016-09-21 13:46:32 +0800

    masonry

--- testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/masonry/MasonryVCTL.m
+++ testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/masonry/MasonryVCTL.m
@@ -41,7 +41,9 @@
 
 - (void)fun1
 {
+    //  下边2个都可以
     _bottomConstraint.offset(-90);
+    _bottomConstraint.offset = -90;
 }
 
 
@@ -61,13 +63,23 @@
 {
     NSArray *a1 = [_v1 mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.view.mas_left).with.offset(20);
-        make.right.equalTo(self.view.mas_right).with.offset(-40);
+        
+        // 距离约束
+        make.right.offset = self.view.mas_right.layoutAttribute-40;
+//        make.right.equalTo(self.view.mas_right).with.offset(-40);
+        
+        
+        // 用property
         _bottomConstraint = make.bottom.equalTo(self.view.mas_bottom);
         _bottomConstraint.with.offset(-50);
-        make.height.equalTo(@60);
+        
+        
+        // 固定 高度
+//        make.height.equalTo(@60);
+        make.height.offset = 60;
     }];
     
-//    NSLog(@"a1 = %@", a1);
+//    NSLog(@"a1 = %@", a1);   // 有4个约束
 }
 
 @end

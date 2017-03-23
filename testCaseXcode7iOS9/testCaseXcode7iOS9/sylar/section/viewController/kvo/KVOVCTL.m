39d21be2fbb28435fcb45d722ed501350966d587
sylar <sylar@Sylar-Mac-Mini-2.local>
2017-03-23 17:09:40 +0800

    kvo

--- testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/kvo/KVOVCTL.m
+++ testCaseXcode7iOS9/testCaseXcode7iOS9/sylar/section/viewController/kvo/KVOVCTL.m
@@ -8,6 +8,8 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 #import "KVOVCTL.h"
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+NSString * const keyKVO1 = @"self.content.text";
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 @interface KVOVCTL ()
 
 @property (nonatomic, strong) NSString *s1;
@@ -21,6 +23,7 @@
 - (void)dealloc
 {
     NSLog(@"dealloc KVOVCTL");
+    [self removeObserver:self forKeyPath:keyKVO1];  // 没有这个会crash
 }
 
 - (void)viewDidLoad
@@ -34,9 +37,12 @@
 {
     _s1 = @"init s1";
     
-//    [self addObserver:self forKeyPath:@"self.s1" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
+    // keyPath必须传 self.s1   _s1没有用
+    // 修改的时候必须用self.s1   _s1没有用  见btn1,btn2
+    [self addObserver:self forKeyPath:@"self.s1" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
+    [self addObserver:self forKeyPath:@"_s1" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
     
-    [self addObserver:self forKeyPath:@"self.content.text" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
+//    [self addObserver:self forKeyPath:keyKVO1 options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
     
     self.s1 = @"desef";
 }
@@ -50,6 +56,7 @@
 {
     self.s1 = @"btn s1";
     _content.text = @"bbb1";
+    NSLog(@"sylar :  address = %p", _content.text);  // 相同地址也会出发kvo
 }
 
 - (IBAction) btn2:(id)sender
